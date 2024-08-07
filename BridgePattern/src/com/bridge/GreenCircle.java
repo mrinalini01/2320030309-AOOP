057aece79d64a56ece267945b702789edce8f843
itzmr <itzmr@Mrinalini>
2024-08-07 21:19:10 +0530

    Shapes and colours

---
+++ BridgePattern/src/com/bridge/GreenCircle.java
@@ -0,0 +1,10 @@
+package com.bridge;
+
+public class GreenCircle implements DrawAPI {
+	
+	public void drawCircle(int radius, int x,int y) {
+		System.out.println("Drawing green circle with radius " + radius + " at (" + x + "," + y + ")");
+
+	}
+}
+
---
+++ BridgePattern/src/com/bridge/RedCircle.java
@@ -0,0 +1,10 @@
+package com.bridge;
+
+public class RedCircle implements DrawAPI {
+	
+	public void drawCircle(int radius, int x, int y){
+		System.out.println("Drawing red circle with radius " + radius + " at (" + x + "," + y + ")");
+
+	}
+}
+
